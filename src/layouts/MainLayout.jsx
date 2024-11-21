7c976828de4aaffb41bb165ebbb19e79ed6d964c
ahnaf4d <ahnaftahmid802@gmail.com>
2024-11-21 15:33:42 +0600

    navbar and footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div className="font-inter">
             <Navbar></Navbar>
-            <Outlet></Outlet>
-            <h2>Footer</h2>
+            <div className="min-h-screen">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
